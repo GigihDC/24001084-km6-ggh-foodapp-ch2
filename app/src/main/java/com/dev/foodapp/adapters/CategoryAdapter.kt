fcec92eceb961cdf2eaa55be3da0fb401a3424de
GigihDC <112244673+GigihDC@users.noreply.github.com>
2024-03-08 23:35:36 +0700

    Finished feature home

---
+++ app/src/main/java/com/dev/foodapp/adapters/CategoryAdapter.kt
@@ -0,0 +1,40 @@
+package com.dev.foodapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dev.foodapp.databinding.ItemCategoryBinding
+import com.dev.foodapp.models.Category
+
+class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+
+    private val data = mutableListOf<Category>()
+
+    fun submitData(items: List<Category>) {
+        data.addAll(items)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        return CategoryViewHolder(
+            ItemCategoryBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    class CategoryViewHolder(private val binding: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Category) {
+            binding.tvCategoryName.text = item.name
+            binding.ivCategoryImage.setImageResource(item.image)
+        }
+    }
+}
